97950d88df51ceebc9b755b64317f526b7e24928
Tsarouchas <tsarouchasa@kotsovolos.gr>
2021-10-18 16:49:09 +0300

    add short circuiting and nullish coalescing operator

--- 06-conditions/conditions.js
+++ 06-conditions/conditions.js
@@ -39,3 +39,10 @@ switch (animal) {
 
 // Ternary Expression
 y > x ? console.log("y is greater than x") : console.log("x is greater than y");
+
+// Short circuiting
+console.log(x && y && 5);
+
+// Nullish coalescing operator
+const foo = null ?? "default string";
+console.log(foo);

528bd9c33b0679ed0ebd5e9de8017845119021ee
Thanos Korakas <thanos.korakas@agileactors.com>
2021-10-18 23:49:47 +0300

    feat: added loops exercises

--- excercises/conditions/condition-exercise.js
+++ excercises/conditions/condition-exercise.js
@@ -1,9 +1,9 @@
-// Write a JavaScript program that accept two integers and display the larger
+// Write a program that accept two integers and display the larger
 
-// Write a JavaScript conditional statement to find the sign of product of three numbers. Display an alert box with the specified sign. Go to the editor
+// Write a conditional statement to find the sign of product of three numbers. Display an alert box with the specified sign. Go to the editor
 // Sample numbers : 3, -7, 2
 // Output : The sign is -
 
-// Write a JavaScript conditional statement to sort three numbers. Display an alert box to show the result. Go to the editor
+// Write a conditional statement to sort three numbers. Display an alert box to show the result. Go to the editor
 // Sample numbers : 0, -1, 4
 // Output : 4, 0, -1
---
+++ excercises/loops/loop-exercise.js
@@ -0,0 +1,19 @@
+// Write a for loop that will iterate from 0 to 15. For each iteration, it will check if the current number is odd or even, and display a message to the screen.
+
+/**
+ * Rewrite the following example using a while and do while loops
+ * let sum = 0;​
+ * for (x = 1; x <= 10; x++) {​
+ *    sum += Math.pow(x,2);​
+ * }
+ */
+
+// Write a program which computes the average marks. Then, use this average to determine the corresponding grade.
+// Ranges <60 -> F, <70 -> D, <80 -> C, <90 -> B, <100 -> A
+const students = [
+  ["David", 80],
+  ["Vinoth", 77],
+  ["Divya", 88],
+  ["Ishitha", 95],
+  ["Thomas", 68],
+];
